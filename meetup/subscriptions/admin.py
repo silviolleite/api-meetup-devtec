1e3904489d61dfde6c84e666ddf778373c2616e2
Embrace <silviolleite@gmail.com>
2019-04-17 07:34:20 -0300

    Adicionando a opção de exportar os inscritos para CSV

--- meetup/subscriptions/admin.py
+++ meetup/subscriptions/admin.py
@@ -1,4 +1,7 @@
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
 from django.utils.timezone import now
 
 from meetup.subscriptions.models import Subscription
@@ -17,6 +20,24 @@ class SubscriptionModelAdmin(admin.ModelAdmin):
 
     subscribed_today.short_description = 'inscrito hoje?'
     subscribed_today.boolean = True
+
+    actions = ["export_as_csv"]
+
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Exportar selecionados"
 
 
 admin.site.register(Subscription, SubscriptionModelAdmin)
